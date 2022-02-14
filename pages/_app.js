6c21da1b476daa29afbe49b4fe9ee0f433b7eec2
Fausto Torres <faut.btorres@gmail.com>
2022-02-14 20:33:52 -0300

    adding google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -5,6 +5,23 @@ function MyApp({ Component, pageProps }) {
   return (
     <>
       <Head>
+        {/* <!-- Global site tag (gtag.js) - Google Analytics --> */}
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-3RZW6F5YC8"
+        ></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+
+              gtag('config', 'G-3RZW6F5YC8');
+            `,
+          }}
+        />
+
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
         <link
