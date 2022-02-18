fc34d1d9131b6837574fefe32d2293a7aebbf277
Fausto Torres <faut.btorres@gmail.com>
2022-02-17 21:01:22 -0300

    termino da primeira parte curso nextjs Alura

--- pages/faq.js
+++ pages/faq.js
@@ -1,37 +1,22 @@
-import PageTitle from "../src/components/PageTitle";
-import Link from "../src/components/Link";
+import FAQScreen from "../src/screens/FAQScreen";
+
+export default FAQScreen;
 
 export async function getStaticProps() {
   const FAQ_API_URL =
     "https://gist.githubusercontent.com/omariosouto/0ceab54bdd8182cbd1a4549d32945c1a/raw/578ad1e8e5296fa048e3e7ff6b317f7497b31ad9/alura-cases-faq.json";
   const faq = await fetch(FAQ_API_URL)
-    .then((res) => {
-      return res.json();
+    .then((respostaDoServidor) => {
+      return respostaDoServidor.json();
     })
-    .then((res) => {
-      return res;
+    .then((resposta) => {
+      return resposta;
     });
 
   return {
     props: {
-      qualquer: "teste",
+      qualquercoisa: "que eu passar aqui",
       faq,
-    }, // will be passed to the page component as props
+    },
   };
-}
-
-export default function FAQPage({ faq }) {
-  return (
-    <div>
-      <PageTitle>FAQ - Alura Cases Campanha</PageTitle>
-
-      <h1>Alura Cases - Páginas de Perguntas FAQ</h1>
-      <Link href="/">Ir para a home</Link>
-      <ul>
-        {faq.map(({ answer, question }) => (
-          <li key={question}>{question}</li>
-        ))}
-      </ul>
-    </div>
-  );
 }
